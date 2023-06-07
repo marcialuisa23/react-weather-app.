bca55e13843deb6da0281eebe97b156339a276d9
marcialuisa23 <marcia7monteiro@gmail.com>
2023-06-07 10:27:12 +0100

    adds my portfolio url

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Hello World!
+      <h1>Hello World!</h1>
       <footer>
-        This project was coded by <a href=""> Marcia Monteiro </a> and is {""}{" "}
+        This project was coded by{" "}
+        <a href="https://incandescent-semolina-99a056.netlify.app/">
+          {" "}
+          Marcia Monteiro{" "}
+        </a>{" "}
+        and is {""}{" "}
         <a href="https://github.com/marcialuisa23/react-weather-app.">
           {" "}
           open-sourced on GitHub{" "}
