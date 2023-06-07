a23b8edec0d827833836f4c67450c493d2420693
marcialuisa23 <marcia7monteiro@gmail.com>
2023-06-07 10:46:26 +0100

    adds bootstrap

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://incandescent-semolina-99a056.netlify.app/">
-          {" "}
-          Marcia Monteiro{" "}
-        </a>{" "}
-        and is {""}{" "}
-        <a href="https://github.com/marcialuisa23/react-weather-app.">
-          {" "}
-          open-sourced on GitHub{" "}
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Hello World!</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://incandescent-semolina-99a056.netlify.app/">
+            {" "}
+            Marcia Monteiro{" "}
+          </a>{" "}
+          and is {""}{" "}
+          <a href="https://github.com/marcialuisa23/react-weather-app.">
+            {" "}
+            open-sourced on GitHub{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
