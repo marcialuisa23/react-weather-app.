73fc088a6a19d2f1fd8c8aa7fb4cd938d6d61092
marcialuisa23 <marcia7monteiro@gmail.com>
2023-06-07 10:22:13 +0100

    adds GitHub url

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      Hello World!
+      <footer>
+        This project was coded by <a href=""> Marcia Monteiro </a> and is {""} <a href="https://github.com/marcialuisa23/react-weather-app."> open-sourced on GitHub </a> 
+
+
+      </footer>
+      
       
     </div>
   );
