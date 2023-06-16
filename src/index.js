76ca2b56152f046ce21bdb3680b3cbdb346faa2f
marcialuisa23 <marcia7monteiro@gmail.com>
2023-06-16 10:38:23 +0100

    creates weather component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
+import Weather from "./Weather";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
